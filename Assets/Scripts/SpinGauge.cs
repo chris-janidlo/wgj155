8d2c532f2661fdb19125822c36e18dd247c64566
crassSandwich <christopherjanidlo@gmail.com>
2020-06-30 23:30:40 -0400

    Spin gauge visual

---
+++ Assets/Scripts/SpinGauge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityAtoms;
+using crass;
+
+// TODO: title for this in the UI
+// FIXME: shouldn't have to specify initial value for VisualSpin; atoms should just auto raise their initial values
+public class SpinGauge : MonoBehaviour
+{
+    public TransitionableFloat VisualSpin;
+    public Image GaugeImage;
+
+    void Awake ()
+    {
+        VisualSpin.AttachMonoBehaviour(this);
+    }
+
+    void Update ()
+    {
+        GaugeImage.fillAmount = (VisualSpin.Value - Spin.MIN) / (Spin.MAX - Spin.MIN);
+    }
+
+    public void OnSpinChange (Spin spin)
+    {
+        VisualSpin.StartTransitionTo(spin);
+    }
+}
